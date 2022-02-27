f6dc86c4de95a13a69618e051d0d2a04d57f82b1
栗嘉男 <574980606@qq.com>
2022-02-28 00:50:52 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import createMemo from './hooks/state/createMemo';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Fibonacci = (n: number, ac1: number = 1, ac2: number = 1): number => {
+  if (n <= 1) {
+    return ac2;
+  }
+  return Fibonacci(n - 1, ac2, ac1 + ac2);
+};
 
+const useFibonacci = createMemo(Fibonacci);
+
+const App: React.FC = () => {
+  const num = useFibonacci(1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <div className="App">{(Fibonacci(1000) === num) + ''}</div>
+    </>
+  );
+};
 
-export default App
+export default App;
